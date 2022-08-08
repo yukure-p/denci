51f9d152688dc6b7b50720ee57e32a43a12bfa9b
ece <yukuree@gmail.com>
2022-08-08 11:43:57 +0900

    js修正

--- custom/js/common.js
+++ custom/js/common.js
@@ -83,3 +83,30 @@ var scrooll = function scrooll() {
 };
 
 scrooll();
+
+var tab = function tab() {
+  document.addEventListener('DOMContentLoaded', function () {
+    // タブに対してクリックイベントを適用
+    var tabs = document.getElementsByClassName('tab');
+
+    for (var i = 0; i < tabs.length; i++) {
+      tabs[i].addEventListener('click', tabSwitch, false);
+    } // タブをクリックすると実行する関数
+
+
+    function tabSwitch() {
+      // タブのclassの値を変更
+      document.getElementsByClassName('is-active')[0].classList.remove('is-active');
+      this.classList.add('is-active'); // コンテンツのclassの値を変更
+
+      document.getElementsByClassName('is-show')[0].classList.remove('is-show');
+      var arrayTabs = Array.prototype.slice.call(tabs);
+      var index = arrayTabs.indexOf(this);
+      document.getElementsByClassName('panel')[index].classList.add('is-show');
+    }
+
+    ;
+  }, false);
+};
+
+tab();
--- src/js/common.js
+++ src/js/common.js
@@ -85,3 +85,26 @@ window.addEventListener('DOMContentLoaded', () => {
 scrooll();
 
 
+const tab = () => {
+document.addEventListener('DOMContentLoaded', function(){
+  // タブに対してクリックイベントを適用
+  const tabs = document.getElementsByClassName('tab');
+  for(let i = 0; i < tabs.length; i++) {
+    tabs[i].addEventListener('click', tabSwitch, false);
+  }
+
+  // タブをクリックすると実行する関数
+  function tabSwitch(){
+    // タブのclassの値を変更
+    document.getElementsByClassName('is-active')[0].classList.remove('is-active');
+    this.classList.add('is-active');
+    // コンテンツのclassの値を変更
+    document.getElementsByClassName('is-show')[0].classList.remove('is-show');
+    const arrayTabs = Array.prototype.slice.call(tabs);
+    const index = arrayTabs.indexOf(this);
+    document.getElementsByClassName('panel')[index].classList.add('is-show');
+  };
+}, false);
+
+};
+tab();
